94f3b1c165d6b36a9f02bcf55b3e3f13b6540798
sidddddd-18 <siddharth.sadhwani@gmail.com>
2020-04-29 21:08:04 +0530

    application linked

--- PatentDB/db.py
+++ PatentDB/db.py
@@ -15,25 +15,40 @@ app.secret_key='yippi k yay'
 def index():
 	if request.method == "POST":
 		details = request.form
-		firstName = details['username']
-		password = details['password']
-		cur = mysql.connection.cursor()
-		cur.execute('''SELECT * FROM users WHERE Username=(%s) AND Password=(%s)''',(firstName,password))
-		result=cur.fetchone()
-		print(result)
-		if result==None:
-			flash(" Try Again!")
-			return render_template('index.html')
+		if len(details)==2:
+			firstName = details['username']
+			password = details['password']
+			cur = mysql.connection.cursor()
+			cur.execute('''SELECT * FROM users WHERE Username=(%s) AND Password=(%s)''',(firstName,password))
+			result=cur.fetchone()
+			print(result)
+			if result==None:
+				flash(" Try Again!")
+				return render_template('index.html')
+			else:
+				role=result['designation']
+				if role=="Attorney":
+					return redirect(url_for('dashAtt'))
+				elif role=="Applicant":
+					return redirect(url_for('dashApp'))
+				elif role=="Patentee":
+					return redirect(url_for('dashPat'))
+				elif role=="Examiner":
+					return redirect(url_for('dashExa'))
 		else:
-			role=result['designation']
-			if role=="Attorney":
-				return redirect(url_for('dashAtt'))
-			elif role=="Applicant":
-				return redirect(url_for('dashApp'))
-			elif role=="Patentee":
-				return redirect(url_for('dashPat'))
-			elif role=="Examiner":
-				return redirect(url_for('dashExa'))
+			Register_name=details['Register_name']
+			password=details['enter_pass']
+			role=details['role']
+			confirm=details['confirm']
+			if confirm==password and (role=="Examiner" or role=="Attorney" or role=="Patentee" or role=="Applicant"):
+				cur = mysql.connection.cursor()
+				cur.execute('''INSERT INTO users(Username,Password,designation) VALUES (%s,%s,%s)''',(Register_name,password,role))
+				mysql.connection.commit()
+				flash("Success")
+				return render_template('index.html')
+			else:
+				flash(" Try Again!")
+				return render_template('index.html')
 		mysql.connection.commit()
 		cur.close()
 		return render_template('index.html')
@@ -54,7 +69,12 @@ def dashExa():
 @app.route('/dash_patentee',methods=['GET','POST'])
 def dashPat():
 		return render_template('dash_patentee.html')
-
-
+@app.route('/application',methods=['GET','POST'])
+def appli():
+	if request.method == "POST":
+		details = request.form
+		firstName
+		return render_template('application.html')
+	return render_template('application.html')
 if __name__ == '__main__':
 	app.run()
