0853bb4883a00a005e3ef44955a13a9e4b98c86d
sidddddd-18 <siddharth.sadhwani@gmail.com>
2020-04-29 19:44:56 +0530

    login works

--- PatentDB/db.py
+++ PatentDB/db.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request,redirect,url_for
+from flask import Flask, render_template, request,redirect,url_for,flash
 from flask_mysqldb import MySQL
 app = Flask(__name__)
 
@@ -9,29 +9,51 @@ app.config['MYSQL_PASSWORD'] = 'siddharth18'
 app.config['MYSQL_DB'] = 'patentdb'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
+app.secret_key='yippi k yay'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == "POST":
 		details = request.form
 		firstName = details['username']
-		lastName = details['password']
+		password = details['password']
 		cur = mysql.connection.cursor()
-		# cur.execute("SELECT Password FROM users WHERE Username=(%s)",(firstName))
-		# result=cur.fetchone()
-		# if result[0]['Password']==NULL:
-		# 	print("hello")
-		# elif result[0]['Password']==lastName:
-		# 	print("wow")
+		cur.execute('''SELECT * FROM users WHERE Username=(%s) AND Password=(%s)''',(firstName,password))
+		result=cur.fetchone()
+		print(result)
+		if result==None:
+			flash(" Try Again!")
+			return render_template('index.html')
+		else:
+			role=result['designation']
+			if role=="Attorney":
+				return redirect(url_for('dashAtt'))
+			elif role=="Applicant":
+				return redirect(url_for('dashApp'))
+			elif role=="Patentee":
+				return redirect(url_for('dashPat'))
+			elif role=="Examiner":
+				return redirect(url_for('dashExa'))
 		mysql.connection.commit()
 		cur.close()
-
-		return redirect(url_for('dashA'))
+		return render_template('index.html')
 	return render_template('index.html')
 
 @app.route('/dash_applicant',methods=['GET','POST'])
-def dashA():
+def dashApp():
 		return render_template('dash_applicant.html')
+
+@app.route('/dash_attorney',methods=['GET','POST'])
+def dashAtt():
+		return render_template('dash_attorney.html')
+
+@app.route('/dash_examiner',methods=['GET','POST'])
+def dashExa():
+		return render_template('dash_examiner.html')
+
+@app.route('/dash_patentee',methods=['GET','POST'])
+def dashPat():
+		return render_template('dash_patentee.html')
 
 
 if __name__ == '__main__':
